1069afd9b95ee0880b4a694cb664ff34f5a7482d
RammarasFTW <33032108+RammarasFTW@users.noreply.github.com>
2018-03-13 10:22:09 +0000

    ZombieGame1

---
+++ ZombieShooter/Assets/Scripts/Explosion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+    public float destroyTime = 0.7f;
+	// Use this for initialization
+	void Start () {
+        Invoke("Die", destroyTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Destroy(gameObject);
+	}
+}
---
+++ ZombieShooter/Assets/Scripts/ZombieBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieBehaviour : MonoBehaviour {
+    public int health = 10;
+    public GameObject explosionPrefab;
+    public float adjustExplosionAngle = 0.0f;
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Quaternion newRot = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + adjustExplosionAngle);
+            Instantiate(explosionPrefab, transform.position, newRot);
+            Destroy(gameObject);
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
